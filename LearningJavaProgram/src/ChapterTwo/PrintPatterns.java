2e5a7b554672895a903979a92c03de5db02e0c1e
Claude Christ <40432185+ChClaude@users.noreply.github.com>
2018-10-31 22:06:44 +0200

    My first commit

---
+++ LearningJavaProgram/src/ChapterTwo/PrintPatterns.java
@@ -0,0 +1,23 @@
+package ChapterTwo;
+
+/**
+ * PrintPatterns.java
+ * This program prints the pattern of a rectangle, ellipse, cross and losange
+ * @author Claude C DE-TCHAMBILA
+ * Date: 31 October 2018
+ */
+public class PrintPatterns {
+
+    public static void main(String[] args) {
+        System.out.printf("*********\t   ***   \t  *  \t    *    %n");
+        System.out.printf("*       *\t *     * \t *** \t   * *   %n");
+        System.out.printf("*       *\t*       *\t*****\t  *   *  %n");
+        System.out.printf("*       *\t*       *\t  *  \t *     * %n");
+        System.out.printf("*       *\t*       *\t  *  \t*       *%n");
+        System.out.printf("*       *\t*       *\t  *  \t *     * %n");
+        System.out.printf("*       *\t*       *\t  *  \t  *   *  %n");
+        System.out.printf("*       *\t *     * \t  *  \t   * *   %n");
+        System.out.printf("*********\t   ***   \t  *  \t    *    %n");
+    }
+
+}
