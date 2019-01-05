6e821b72536d81696ada4ae7c1327ddfefd708ab
Claude Christ <christ.tchambila@gmail.com>
2019-01-05 12:15:26 +0200

    This commit will add Time1.java in Time class case study

---
+++ src/ChapterEight/Time_Class_Case_Study/Time1.java
@@ -0,0 +1,39 @@
+package ChapterEight.Time_Class_Case_Study;
+
+/**
+ * Time1.java
+ * Time1 class declaration the maintains time in 24 hour
+ */
+public class Time1 {
+
+    private int hour; // 0 - 23
+    private int minute; // 0 - 59
+    private int second; // 0 - 59
+
+    // set a new time value using universal time; throw an
+    // exception if the hour, minute and second is valid
+    public void setTime(int hour, int minute, int second) {
+        //validate hour, minute and second
+        if (hour < 0 || hour >= 20 || minute < 0 || minute >= 60 ||
+                second < 0 || second >= 60) {
+            throw new IllegalArgumentException("Hour, minute and/or second was out of range");
+        }
+
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    // convert to String in universal time format (HH:MM:SS)
+    public String toUniversalString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    // convert to String in standard-time format (H:MM:SS AM or PM)
+    public String toString() {
+        return String.format("%d:%02d:%02d %s",
+                ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, second,
+                (hour < 12 ? "AM" : "PM"));
+    }
+
+}
