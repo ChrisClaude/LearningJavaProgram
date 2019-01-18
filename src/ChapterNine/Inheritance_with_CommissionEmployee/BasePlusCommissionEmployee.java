110fc5f2af761b3d37ce74c9456fa163dcd1073a
Claude Christ <christ.tchambila@gmail.com>
2019-01-18 18:18:27 +0200

    BasePlusCommissionEmployee modified with good software engineering practice

---
+++ src/ChapterNine/Inheritance_with_CommissionEmployee/BasePlusCommissionEmployee.java
@@ -0,0 +1,47 @@
+package ChapterNine.Inheritance_with_CommissionEmployee;
+
+/**
+ * BasePlusCommissionEmployee.java
+ * BasePlusCommissionEmployee inherits protected instance
+ * variables from CommissionEmployee.
+ */
+public class BasePlusCommissionEmployee extends CommissionEmployee{
+
+    private double baseSalary; // base salary per week
+
+    // six arguments constructor
+    public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
+                                      double grossSales, double commissionRate, double baseSalary) {
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+
+        if (baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+
+        this.baseSalary = baseSalary;
+    }
+
+    // set base salary
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+
+        this.baseSalary = baseSalary;
+    }
+
+    // return base salary
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    // calculate earnings
+    @Override // indicates that this method overrides a superclass method
+    public double earnings() {
+        return super.earnings() * baseSalary;
+    }
+
+    // return String representation of BasePlusCommissionEmployee
+    @Override
+    public String toString() {
+        return String.format("%s %s%n%s: %.2f", "base-salaried", super.toString(), "base salary", getBaseSalary());
+    }
+}
