65b749a45650cd0f299140e41c9cd3fe722bb413
Claude Christ <christ.tchambila@gmail.com>
2019-01-22 20:23:42 +0200

    Chapter_10, Polymorphism_and_Interfaces, Payable.java completed

---
+++ src/Chapter_10/Polymorphism_and_Interfaces/Payable.java
@@ -0,0 +1,9 @@
+package Chapter_10.Polymorphism_and_Interfaces;
+
+/**
+ * Payable.java
+ * Payable interface declaration
+ */
+public interface Payable {
+    double getPaymentAmount(); // calculate payment; no implementation
+}
