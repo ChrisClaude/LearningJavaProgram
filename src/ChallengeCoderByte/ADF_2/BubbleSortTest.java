11417c1a21edf7cf70915225bf00a858085a221c
Claude Christ <christ.tchambila@gmail.com>
2019-02-27 16:38:12 +0200

    BbbleSortTest updated, can perform better with already sorted array

--- src/ChallengeCoderByte/ADF_2/BubbleSortTest.java
+++ src/ChallengeCoderByte/ADF_2/BubbleSortTest.java
@@ -16,6 +16,18 @@ public class BubbleSortTest {
         bubbleSort(numbers);
         System.out.println("After sorting");
         displayArray(numbers);
+
+        // sorting numbers_2 array
+        // NB: number_2 is already sorted, we're checking the bubbleSort method performance
+        int[] numbers_2 = {13, 18, 23, 33, 41, 52};
+        System.out.println();
+        System.out.println("Before sorting");
+        displayArray(numbers_2);
+        System.out.println();
+
+        bubbleSort(numbers_2);
+        System.out.println("After sorting");
+        displayArray(numbers_2);
     }
 
     /**
@@ -25,21 +37,24 @@ public class BubbleSortTest {
      * @return the given array sorted in ascending order
      */
     public static int[] bubbleSort(int [] array) {
+        boolean arrayNotSorted = true;
         int count = 0;
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 1; i < array.length && arrayNotSorted; i++) {
             int temp;
+            arrayNotSorted = false;
 
             for (int j = 0; j < array.length - i; j++) {
                 if (array[j] > array[j+1]) {
                     temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    arrayNotSorted = true;
                 }
                 ++count;
             }
         }
 
-        System.out.println(count);
+        System.out.println("Count for performance: " + count);
         return array;
     }
 
