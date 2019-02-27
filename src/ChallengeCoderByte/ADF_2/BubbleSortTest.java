04f00728c273648afcd648e13fb35500903a1411
Claude Christ <christ.tchambila@gmail.com>
2019-02-27 15:32:19 +0200

    Bubble sort version one, the loop is doing more comparison than necessary

--- src/ChallengeCoderByte/ADF_2/BubbleSortTest.java
+++ src/ChallengeCoderByte/ADF_2/BubbleSortTest.java
@@ -7,7 +7,48 @@ use two different sorting algorithms.
 public class BubbleSortTest {
 
     public static void main(String[] args) {
+        int[] numbers = {9, 5, 7, 3, 2, 8, 16};
 
+        System.out.println("Before sorting");
+        displayArray(numbers);
+        System.out.println();
+
+        bubbleSort(numbers);
+        System.out.println("After sorting");
+        displayArray(numbers);
+    }
+
+    /**
+     * bubbleSort is a sorting algorithm,
+     * it sorts a given array in a descending order.
+     * @param array - this an array of numbers to be sorted
+     * @return the given array sorted in ascending order
+     */
+    public static int[] bubbleSort(int [] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            int temp;
+
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+                ++count;
+            }
+        }
+
+        System.out.println(count);
+        return array;
+    }
+
+    // display array method
+    public static void displayArray(int[] arr) {
+        for (int x : arr) {
+            System.out.printf("%d ", x);
+        }
+        System.out.println();
     }
 
 }
