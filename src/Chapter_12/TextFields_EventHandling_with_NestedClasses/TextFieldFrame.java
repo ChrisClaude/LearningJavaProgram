937bbbbce9f914441e9112c279fd86d56f1b6746
Claude Christ <christ.tchambila@gmail.com>
2019-03-09 17:19:59 +0200

    Chapter_12, TextFields and event handling completed

--- src/Chapter_12/TextFields_EventHandling_with_NestedClasses/TextFieldFrame.java
+++ src/Chapter_12/TextFields_EventHandling_with_NestedClasses/TextFieldFrame.java
@@ -1,4 +1,79 @@
 package Chapter_12.TextFields_EventHandling_with_NestedClasses;
 
-public class TextFieldFrame {
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+/**
+ * TextFieldFrame.java
+ * JTextFields and JPasswordFields
+ */
+public class TextFieldFrame extends JFrame{
+
+    private final JTextField textField1; // text field with set size
+    private final JTextField textField2; // text field with text
+    private final JTextField textField3; // text field with text and size
+    private final JPasswordField passwordField; // password field with text
+
+    public TextFieldFrame() {
+        super("Testing JTextField and JPasswordField");
+        setLayout(new FlowLayout());
+
+        // construct text field 10 columns
+        textField1 = new JTextField(10);
+        add(textField1); // add textField1 to JFrame
+
+        // construct text field with default text
+        textField2 = new JTextField("Enter text here");
+        add(textField2); // add textField2 to JFrame
+
+        // construct text field with default text and 21 columns
+        textField3 = new JTextField("Uneditable text field", 21);
+        textField3.setEditable(false); // disable editing
+        add(textField3); // add textField3 to JFrame
+
+        // construct password field with default text
+        passwordField = new JPasswordField("Hidden text");
+        add(passwordField); // add passwordField to JFrame
+
+        // register event handlers
+        TextFieldHandler handler = new TextFieldHandler();
+        textField1.addActionListener(handler);
+        textField2.addActionListener(handler);
+        textField3.addActionListener(handler);
+        passwordField.addActionListener(handler);
+    }
+
+    // private inner class for event handling
+    private class TextFieldHandler implements ActionListener {
+        // process text field events
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String string = "";
+
+            // user pressed Enter in JTextField textField1
+            if (e.getSource() == textField1)
+                string = String.format("textField1: %s", e.getActionCommand());
+
+            // user pressed Enter in JTextField textField2
+            else if (e.getSource() == textField2)
+                string = String.format("textField2: %s", e.getActionCommand());
+
+            // user pressed Enter in JTextField textField3
+            else if (e.getSource() == textField3)
+                string = String.format("textField3: %s", e.getActionCommand());
+
+            // user pressed Enter in JTextField passwordField
+            else if (e.getSource() == passwordField)
+                string = String.format("passwordField: %s", e.getActionCommand());
+
+            // display JTextField content
+            JOptionPane.showMessageDialog(null, string);
+        }
+    }
+
 }
---
+++ src/Chapter_12/TextFields_EventHandling_with_NestedClasses/TextFieldTest.java
@@ -0,0 +1,14 @@
+package Chapter_12.TextFields_EventHandling_with_NestedClasses;
+
+import javax.swing.*;
+
+public class TextFieldTest {
+
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350, 120);
+        textFieldFrame.setVisible(true);
+    }
+
+}
