509c6e761119f2dff7a59435242713fc1c8141f0
Claude Christ <christ.tchambila@gmail.com>
2019-05-02 22:15:23 +0200

    Chapter_22, BoxLayoutFrame completed

---
+++ src/Chapter_22/Box_Layout_Manager/BoxLayoutDemo.java
@@ -0,0 +1,14 @@
+package Chapter_22.Box_Layout_Manager;
+
+import javax.swing.*;
+
+public class BoxLayoutDemo {
+
+    public static void main(String[] args) {
+        BoxLayoutFrame boxLayoutFrame = new BoxLayoutFrame();
+        boxLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        boxLayoutFrame.setSize(400, 200);
+        boxLayoutFrame.setVisible(true);
+    }
+
+}
