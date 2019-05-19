c8af8803364fd62797b15fee45bbe536899d7de7
Claude Christ <christ.tchambila@gmail.com>
2019-05-19 21:00:33 +0200

    Data structure singlyLinkedList completed

--- src/Data_Structure_and_Algorithms/LinkedList/Singly_Linked_List/SinglyLinkedList.java
+++ src/Data_Structure_and_Algorithms/LinkedList/Singly_Linked_List/SinglyLinkedList.java
@@ -1,4 +1,89 @@
 package Data_Structure_and_Algorithms.LinkedList.Singly_Linked_List;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> {
+
+    private Node<E> head = null; // head node of the list (or null if empty)
+    private Node<E> tail = null; // last node of the list (or null if empty)
+    private int size = 0; // number of nodes in the list
+
+    public SinglyLinkedList() {
+    } // constructs an initially empty list
+
+    // access methods
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() { // returns but does not remove the first element
+        if (isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    public E last() { // returns (but does not remove) the last element
+        if (isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    // update methods
+    public void addFirst(E e) { // adds element e to the font of the list
+        head = new Node<>(e, head); // create and link a new node
+
+        if (size == 0)
+            tail = head; // special case: new node becomes tail also
+
+        size++;
+    }
+
+    public void addLast(E e) { // adds element e to the end of the list
+        Node<E> newest = new Node<>(e, null); // node will eventually be the tail
+
+        if (isEmpty())
+            head = newest;      // special case: previously empty list
+        else
+            tail.setNext(newest); // new node after existing tail
+        tail = newest; // new becomes the tail
+        size++;
+    }
+
+    public E removeFirst() { // removes and returns the first element
+        if (isEmpty()) // nothing to remove
+            return null;
+
+        E answer = head.getElement();
+        head = head.getNext(); // will become null if list had only one node
+        size--;
+
+        if (size == 0)
+            tail = null; // special case as list is now empty
+
+        return answer;
+    }
+
+    //-------------- nested Node class -------------
+    private static class Node<E> {
+
+        private E element; // reference to the element stored at this node
+        private Node<E> next; // reference to the subsequent node in the list
+
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
 }
