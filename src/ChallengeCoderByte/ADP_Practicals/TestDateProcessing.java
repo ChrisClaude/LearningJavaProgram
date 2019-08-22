f4c00f34b37246c3d954e59ef7928b39df776321
Claude Christ <christ.tchambila@gmail.com>
2019-08-22 09:34:09 +0200

    TestDateProcessing added

--- src/ChallengeCoderByte/ADP_Practicals/TestDateProcessing.java
+++ src/ChallengeCoderByte/ADP_Practicals/TestDateProcessing.java
@@ -1,4 +1,37 @@
 package ChallengeCoderByte.ADP_Practicals;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
 public class TestDateProcessing {
+
+    public static void main(String[] args) {
+        System.out.println(determineAge("16-12-1999"));
+        System.out.println(calculateAge(LocalDate.of(1999, 10, 16), LocalDate.now()));
+
+    }
+
+    private static int determineAge(String age) {
+        int bday, bmonth, byear;
+        String[] dob = age.split("\\-");
+        bday = Integer.parseInt(dob[0]);
+        bmonth = Integer.parseInt(dob[1]);
+        byear = Integer.parseInt(dob[2]);
+
+        LocalDate birthday = LocalDate.of(byear, bmonth, bday);
+        LocalDate today = LocalDate.now();
+
+        return (int) ChronoUnit.YEARS.between(birthday, today);
+    }
+
+    public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+        if ((birthDate != null) && (currentDate != null)) {
+            return Period.between(birthDate, currentDate).getYears();
+        } else {
+            return 0;
+        }
+    }
+
+
 }
